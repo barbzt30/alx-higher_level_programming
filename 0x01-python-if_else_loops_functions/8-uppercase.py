e721fb4e5b1ac0e6b7e9000f0d11a769078cac2a
Barbra Wasiwo <b.wasiwo.bw@gmail.com>
2023-11-29 11:46:56 -0800

    Main

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def uppercase(s):
+    result = ""
+    for char in s:
+        if 'a' <= char <= 'z':
+            offset = ord('A') - ord('a')
+            result += "{:c}".format(ord(char) + offset)
+        else:
+            result += char
+
+            print(result)
+
+            if __name__ == "__main__":
+                uppercase("best")
+                uppercase("Best School 98 Battery street")
