e6c82ae9e3067daaad5cbbeae0742954fc04de46
Barbra Wasiwo <b.wasiwo.bw@gmail.com>
2023-12-05 10:47:14 -0800

    Palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 listint_t *reverse_listint(listint_t **head);
-int is_palindrome(listiknt_t **head);
+int is_palindrome(listint_t **head);
 
 /**
  * reverse_listint - Reverses a singly-linked listint_t list
