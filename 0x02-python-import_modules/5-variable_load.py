c88c670ae6243a24670c65aad313f03ea59a25af
barbzt30 <b.wasiwo.bw@gmail.com>
2024-01-18 06:20:14 -0800

    Everything can be imported

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-from variable_load_5 import a
 
+if __name__ == "__main__":
+    """Print the value of variable a from variable_load5."""
+    from variable_load_5 import a
 
-def find_var():
     print(a)
-
-    if __name__ == "__main__":
-        find_var()
