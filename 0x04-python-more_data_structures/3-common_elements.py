edb5088a29172c5c8e61bdb2df1453104f6e93da
barbzt30 <b.wasiwo.bw@gmail.com>
2024-01-24 07:49:49 -0800

    Common elements

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,16 +1,3 @@
 #!/usr/bin/python3
-"""
-the function returns a similar element in sets
-the loop iterates through the first set for condition does the checkin
-abd finally when a similar element is found in its added to the empty set
-"""
-
-
 def common_elements(set_1, set_2):
-    common_set = set()
-
-    for element in set_1:
-        if element in set_2:
-            common_set.add(element)
-
-            return common_set
+    return (set_1 & set_2)
