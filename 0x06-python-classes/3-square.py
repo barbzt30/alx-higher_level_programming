b225071605aab5c628562caf53d6432202b15513
barbzt30 <b.wasiwo.bw@gmail.com>
2024-02-06 09:49:52 -0800

    Area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -16,15 +16,14 @@ class Square:
             if size < 0:
                 raise ValueError('size must be >= 0')
             else:
-                self.__size = size
-            else:
                 raise TypeError('size must be an integer')
+            self.__size = size
 
             def area(self):
                 """
                 finds area of square
                 Returns:
-                the area of the squarr
+                the area of the square
                 """
 
                 return self.__size ** 2
